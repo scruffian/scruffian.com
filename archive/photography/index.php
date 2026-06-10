33b8aead920463872247fb426f6f340b37b1112b
Ben Dwyer <ben@scruffian.com>
2026-06-10 13:47:47 +0100

    Fix photography archive (#15)

--- archive/photography/index.php
+++ archive/photography/index.php
@@ -21,49 +21,43 @@ a:hover      { color: #000000; background-color: #ffffff;}
   <tr>
     <td width="100%">
         <a href="index.php"><img src="title.gif" width="750" height="100" border="0"></a><br />
-<a href="http://www.scruffian.com/">Home&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a><a href="../messages.php">Messages&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a><a href="http://www.scruffian.com/zine/">Zine&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a><a href="../index2.php?page=contact">Contact&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+<a href="http://www.scruffian.com/">Home&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a><a href="../messages.php">Messages&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a><a href="../zine/index.php">Zine&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a><a href="../contact.php">Contact&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
 <br>
     <table cellpadding="0" cellspacing="0" style="border: solid 5px #000000;" >
       <tr>
         <td width="100%">
 <?php
 
-$sub = $_REQUEST["sub"]; // Get the subdirectory from URL
-
+$sub = isset($_REQUEST["sub"]) ? $_REQUEST["sub"] : ""; // Get the subdirectory from URL
 $ext = array("jpg", "png", "jpeg", "gif"); // These are the file extensions that will be displayed
 
-if(isset ($sub)) // This is what will happen if the subdirectory is set in the URL
+if (preg_match('/^[0-9]{6}$/', $sub) && is_dir($sub)) // This is what will happen if the subdirectory is set in the URL
 {
+	$files = array();
+
 	if ($handle = opendir($sub))
 	{
 		while ($file = readdir($handle)) // This reads the directory contents
 		{
 			$localpath = $sub."/".$file;
+			$extension = strtolower(pathinfo($file, PATHINFO_EXTENSION));
  
-			if (is_file($localpath))
+			if (is_file($localpath) && in_array($extension, $ext))
 			{
-				$key = filemtime($localpath).md5($file);
-				$files[$key] = $file;
+				$files[] = $file;
 			}
 		}
-		asort($files);
+		closedir($handle);
+
+		sort($files);
 		foreach ($files as $file)
 		{
-			for($i=0;$i<sizeof($ext);$i++) 
-			if(stristr($file, ".".$ext[$i])) // Find out if the file extensions match those above NOT case sensitive.
-			{
-					if ($file != "." && $file != "..") // Get rid of dot files
-					{
-						$extension = strrchr($file, '.'); // This strips off the extension
-						if($extension !== false)
-						{
-							$filename = substr($file, 0, -strlen($extension));
-						}
-					echo "<img src='$sub/$file' alt='$filename'><br>";
-					}
-			}
+			$filename = pathinfo($file, PATHINFO_FILENAME);
+			$src = htmlspecialchars($sub."/".$file, ENT_QUOTES);
+			$alt = htmlspecialchars($filename, ENT_QUOTES);
+
+			echo "<img src='$src' alt='$alt'><br>";
 		}
-	closedir($handle);
 	}
 }
 ?>
@@ -71,35 +65,31 @@ if(isset ($sub)) // This is what will happen if the subdirectory is set in the U
       </tr>
     </table>
 <?php
+	$files = array();
+
 	if ($handle = opendir('.'))
 	{
 		while ($file = readdir($handle))
 		{
-			$localpath = $file;
- 
-			if (is_dir($localpath))
+			if (preg_match('/^[0-9]{6}$/', $file) && is_dir($file))
 			{
-				$key = filemtime($localpath).md5($file);
-				$files[$key] = $file;
+				$files[] = $file;
 			}
 		}
-		arsort($files);
+		closedir($handle);
+
+		rsort($files);
 		foreach ($files as $file)
 		{
-			if ($file != "." && $file != "..")
-			{
-				if (is_dir($file))
-				{
-					$Year = substr($file, 0, 2);
-					$Month = substr($file, 2, 2);
-					$Day = substr($file, 4, 2);
-					$Date = date("l j F", mktime(0, 0, 0, $Month, $Day, $Year));
-					echo "<a href='?sub=$file'>$Date</a><br />";
-				}
-			}
+			$Year = substr($file, 0, 2);
+			$Month = substr($file, 2, 2);
+			$Day = substr($file, 4, 2);
+			$Date = date("l j F", mktime(0, 0, 0, $Month, $Day, $Year));
+			$link = htmlspecialchars($file, ENT_QUOTES);
+
+			echo "<a href='?sub=$link'>$Date</a><br />";
 		}
    	}
-   	closedir($handle);
 
 ?>
     </td>
