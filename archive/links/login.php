489908e18ad2f567cbf44623e1fa5eda1fcef092
Ben Dwyer <ben@scruffian.com>
2026-06-11 15:35:43 +0100

    Convert archive PHP pages to static HTML

--- archive/links/login.php
+++
@@ -1,4 +0,0 @@
-<form name="pp" method="post" action="check.php">
-<input type="password" name="pass">
-<input type="submit" value="Log In">
-</form>
--- design/pinkscales/mtview.php
+++
@@ -1,5 +0,0 @@
-<?php
-    include('/home/thisis/public_html/news/php/mt.php');
-    $mt = new MT(9, '/home/thisis/public_html/news/mt.cfg');
-    $mt->view();
-?>
